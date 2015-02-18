39d0856bdc0abc14748ff480cc319a79314cab8c
Nikit Waghela <waghelanikit@gmail.com>
2015-02-18 18:40:09 -0500

    documented parser

--- utils/src/main/java/mapred/utils/Parser.java
+++ utils/src/main/java/mapred/utils/Parser.java
@@ -10,6 +10,11 @@ public class Parser {
 	private String cat;
 	private Double val;
 	
+	/**
+	 * Parses the String line and produces Category and Price data
+	 * @param line String
+	 * @return this Parser
+	 */
 	public Parser  parse(String line) {
 		String parts[] = line.trim().split("\t");
 		if(parts.length == 6) {
@@ -27,14 +32,27 @@ public class Parser {
 		return this;
 	}
 	
+	/**
+	 * returns the last parsed category
+	 * @return category String
+	 */
 	public String getCat() {
 		return cat;
 	}
 	
+	/**
+	 * returns the last parsed price
+	 * @return price Double
+	 */
 	public Double getVal() {
 		return val; 
 	}
 	
+	/**
+	 * Returns true if an only if both cat and val are non-null
+	 * else returns false
+	 * @return isValid boolean
+	 */
 	public boolean isValid() {
 		return val != null && cat != null;
 	}
